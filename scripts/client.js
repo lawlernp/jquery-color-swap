4cf14c38a4311151397f69724df3aba25eb861a6
lawlernp <lawlernp@gmail.com>
2020-10-05 13:48:13 -0500

    listening on yellow and delete buttongs

--- scripts/client.js
+++ scripts/client.js
@@ -3,18 +3,32 @@ $(document).ready(onReady);
 function onReady(){
     console.log('hi from jq');
     $("#buttonGenerate").on('click', generate)
+    $('#boxContainer').on('click', '#buttonYellow', yellow);
+    $('#boxContainer').on('click', '#buttonDelete', deleteFunction);
+
+
 }
 let counter = 0
 function generate() {
-    console.log('git generate');
+    console.log('generate');
     counter++
     $("#boxContainer").append(`
-        <div>
+        <div class=red>
             <p>${counter}<p>
             <button id=buttonYellow>Yellow</button>
             <button id=buttonDelete>Delete</button>
         </div>
     `)
+}
+
+function yellow() {
+    console.log('hi from yellow');
+    
+}
+
+function deleteFunction() {
+    console.log('hi from delete');
+    
 }
 
 console.log('hi');
